d65c8a17cddba8b3b3caad5a0e7057321863926d
Simon Eyob <esimon1997@gmail.com>
2020-04-12 20:23:46 -0400

    finished hw

--- app.py
+++ app.py
@@ -36,26 +36,27 @@ app = Flask(__name__)
 def welcome():
     """List all available api routes."""
     return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
+        f"<p>Welcome to the Hawaii weather API!</p>"
+        f"<p>Usage:</p>"
+        f"/api/v1.0/precipitation<br/>Returns a JSON list of percipitation data for the dates between 8/23/16 and 8/23/17<br/><br/>"
+        f"/api/v1.0/stations<br/>Returns a JSON list of the weather stations<br/><br/>"
+        f"/api/v1.0/tobs<br/>Returns a JSON list of the Temperature Observations (tobs) for each station for the dates between 8/23/16 and 8/23/17<br/><br/>"
+        f"/api/v1.0/date<br/>Returns a JSON list of the minimum temperature, the average temperature, and the max temperature for the dates between the given start date and 8/23/17<br/><br/>."
+        f"/api/v1.0/start_date/end_date<br/>Returns a JSON list of the minimum temperature, the average temperature, and the max temperature for the dates between the given start date and end date<br/><br/>."
     )
 
+
+# /api/v1.0/precipitation
+# Convert the query results to a Dictionary using date as the key and tobs as the value.
+# Return the JSON representation of your dictionary.
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # Create our session (link) from Python to the DB
     session = Session(engine)
-
-    sel = [Measurement.date,
-       Measurement.prcp]
-    # Save the query results as a Pandas DataFrame and set the index to the date column
+    sel = [Measurement.date,Measurement.prcp]
     results=session.query(*sel).\
     order_by(Measurement.date).all()
-
     session.close()
 
     # Create a dictionary from the row data and append to a list of all_passengers
@@ -66,6 +67,9 @@ def precipitation():
         all_precipitation.append(precipitation_dict)
     return jsonify(all_precipitation)
 
+
+#/api/v1.0/stations
+#Return a JSON list of stations from the dataset.
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
@@ -77,6 +81,9 @@ def stations():
     return jsonify(all_names)
 
 
+#/api/v1.0/tobs
+#Query the dates and temperature observations of the most active station for the last year of data.
+#Return a JSON list of temperature observations (TOBS) for the previous year.
 @app.route("/api/v1.0/tobs")
 def tobs():
     session = Session(engine)
@@ -92,6 +99,43 @@ def tobs():
         tobs_dict[date] = tobs
         all_tobs.append(tobs_dict)
     return jsonify(all_tobs)
+    session.close()
+
+
+#/api/v1.0/<start>
+#When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+@app.route("/api/v1.0/<start>")
+def startdate(start):
+    session = Session(engine)
+    temp = [func.min(Measurement.tobs),
+       func.max(Measurement.tobs),
+       func.avg(Measurement.tobs)]
+    start_date=session.query(Measurement.date).filter(Measurement.date == start)
+    if start_date[0][0]==start:
+        results=session.query(*temp).filter(Measurement.date > start).all()
+        result=[f"Min TEMP = {results[0][0]}",f"MAX TEMP = {results[0][1]}",f"AVG TEMP = {results[0][2]}"]
+        temp_ans=list(np.ravel(result))
+        return jsonify(temp_ans)
+    # else:
+    #     return jsonify({"error": f"Date chosen {start} does not exist in database"}), 404
+
+
+
+# /api/v1.0/<start>/<end>
+# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+# When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start,end):
+    session = Session(engine)
+    temp = [func.min(Measurement.tobs),
+       func.max(Measurement.tobs),
+       func.avg(Measurement.tobs)]
+    start_date=session.query(Measurement.date).filter(Measurement.date == start).filter(Measurement.date ==end)
+    results=session.query(*temp).filter(Measurement.date > start).filter(Measurement.date < end).all()
+    result=[f"Min TEMP = {results[0][0]}",f"MAX TEMP = {results[0][1]}",f"AVG TEMP = {results[0][2]}"]
+    temp_ans=list(np.ravel(result))
+    return jsonify(temp_ans)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
